93238bbc274cfe25f47d6bf3624968615f2efd80
MagretBro <magretbro@gmail.com>
2024-03-21 20:18:43 +0700

    animation cat & bowl

--- src/app/components/catpage/catpage.component.ts
+++ src/app/components/catpage/catpage.component.ts
@@ -1,3 +1,6 @@
+
+
+// ВЕРСИЯ ПЕРЕД ГИМИНИ
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./catpage.component.scss']
 })
 export class CatpageComponent {
+
+  showBowlVisible: boolean = false;
+
+  toggleBowlVisibility() {
+    this.showBowlVisible = !this.showBowlVisible;
+  }
+
+
   showBowl() {
     const bowl = document.querySelector('.bowl');
     if (bowl) {
@@ -17,3 +28,28 @@ export class CatpageComponent {
   }
 
 }
+
+
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-catpage',
+//   templateUrl: './catpage.component.html',
+//   styleUrls: ['./catpage.component.scss']
+// })
+// export class CatpageComponent {
+//   showBowl() {
+//     const bowl = document.querySelector('.bowl');
+//     if (bowl) {
+//       bowl.classList.remove('hidden');
+//       setTimeout(() => {
+//         bowl.classList.add('hidden');
+//       }, 3000)
+//     }
+//   }
+
+// }
