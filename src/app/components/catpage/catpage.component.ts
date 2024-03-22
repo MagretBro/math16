2c0f640be7562adf8df24e2097898abddd20e14e
MagretBro <magretbro@gmail.com>
2024-03-22 21:32:20 +0700

    animation cat

--- src/app/components/catpage/catpage.component.ts
+++ src/app/components/catpage/catpage.component.ts
@@ -1,6 +1,3 @@
-
-
-// ВЕРСИЯ ПЕРЕД ГИМИНИ
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './catpage.component.html',
   styleUrls: ['./catpage.component.scss']
 })
+
 export class CatpageComponent {
 
   showBowlVisible: boolean = false;
@@ -16,22 +14,70 @@ export class CatpageComponent {
     this.showBowlVisible = !this.showBowlVisible;
   }
 
-
   showBowl() {
     const bowl = document.querySelector('.bowl');
     if (bowl) {
       bowl.classList.remove('hidden');
-      setTimeout(() => {
-        bowl.classList.add('hidden');
-      }, 3000)
     }
   }
 
+  moveKitten() {
+    const kitten = document.querySelector('.kitten');
+    if (kitten) {
+      // console.log('Kitten is moving down and scaling up');
+      kitten.classList.add('move-down-400');
+      kitten.classList.add('scale-up-30');
+      // console.log(kitten);
+    }
+  }
+  
 }
 
 
 
+// ВЕРСИЯ ПЕРЕД ГИМИНИ
 
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-catpage',
+//   templateUrl: './catpage.component.html',
+//   styleUrls: ['./catpage.component.scss']
+// })
+// export class CatpageComponent {
+
+//   showBowlVisible: boolean = false;
+
+//     toggleBowlVisibility() {
+//       this.showBowlVisible = !this.showBowlVisible;
+//     }
+//   showBowl() {
+//     const bowl = document.querySelector('.bowl');
+//     if (bowl) {
+//       bowl.classList.remove('hidden');
+//       const kitten = document.querySelector('.kitten');
+//       if (kitten) {
+//         kitten.classList.add('move-down');
+//         // kitten.classList.add('scale-up');
+//         console.log('Kitten is moved down and scaled up');
+//       } else {
+//         console.log('Kitten element not found');
+//       }
+//     } else {
+//       console.log('Bowl element not found');
+//     }
+//       }
+//     }
+
+
+
+
+
+
+
+
+
+/////// 1 v
 
 
 // import { Component } from '@angular/core';
