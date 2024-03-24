b13aef49c63c7ea80146f9d71b213723a9973db8
MagretBro <magretbro@gmail.com>
2024-03-24 12:47:33 +0700

    animation cat&bowl

--- src/app/components/catpage/catpage.component.ts
+++ src/app/components/catpage/catpage.component.ts
@@ -1,3 +1,6 @@
+
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,37 +8,57 @@ import { Component } from '@angular/core';
   templateUrl: './catpage.component.html',
   styleUrls: ['./catpage.component.scss']
 })
-
 export class CatpageComponent {
 
   showBowlVisible: boolean = false;
 
-  toggleBowlVisibility() {
-    this.showBowlVisible = !this.showBowlVisible;
-  }
-
+    toggleBowlVisibility() {
+      this.showBowlVisible = !this.showBowlVisible;
+    }
   showBowl() {
     const bowl = document.querySelector('.bowl');
     if (bowl) {
       bowl.classList.remove('hidden');
+      const kitten = document.querySelector('.kitten');
+      if (kitten) {
+        kitten.classList.add('move-down-400');
+        // kitten.classList.add('scale-up');
+        console.log('Kitten is moved down and scaled up');
+      } else {
+        console.log('Kitten element not found');
+      }
+    } else {
+      console.log('Bowl element not found');
     }
-  }
+      }
+    }
 
-  moveKitten() {
-    const kitten = document.querySelector('.kitten');
-    if (kitten) {
-      // console.log('Kitten is moving down and scaling up');
-      kitten.classList.add('move-down-400');
-      kitten.classList.add('scale-up-30');
-      // console.log(kitten);
-    }
-  }
+//   showBowl() {
+//     const bowl = document.querySelector('.bowl');
+//     if (bowl) {
+//       bowl.classList.remove('hidden');
+//     }
+//   }
+
+//   moveKitten() {
+//     const kitten = document.querySelector('.kitten');
+//     if (kitten) {
+//       // console.log('Kitten is moving down and scaling up');
+//       kitten.classList.add('move-down-400');
+//       kitten.classList.add('scale-up-30');
+//       // console.log(kitten);
+//     }
+//   }
   
-}
 
 
 
-// ВЕРСИЯ ПЕРЕД ГИМИНИ
+
+
+
+
+
+// is working
 
 // import { Component } from '@angular/core';
 
@@ -44,58 +67,33 @@ export class CatpageComponent {
 //   templateUrl: './catpage.component.html',
 //   styleUrls: ['./catpage.component.scss']
 // })
+
 // export class CatpageComponent {
 
 //   showBowlVisible: boolean = false;
 
-//     toggleBowlVisibility() {
-//       this.showBowlVisible = !this.showBowlVisible;
-//     }
+//   toggleBowlVisibility() {
+//     this.showBowlVisible = !this.showBowlVisible;
+//   }
+
 //   showBowl() {
 //     const bowl = document.querySelector('.bowl');
 //     if (bowl) {
 //       bowl.classList.remove('hidden');
-//       const kitten = document.querySelector('.kitten');
-//       if (kitten) {
-//         kitten.classList.add('move-down');
-//         // kitten.classList.add('scale-up');
-//         console.log('Kitten is moved down and scaled up');
-//       } else {
-//         console.log('Kitten element not found');
-//       }
-//     } else {
-//       console.log('Bowl element not found');
-//     }
-//       }
-//     }
-
-
-
-
-
-
-
-
-
-/////// 1 v
-
-
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-catpage',
-//   templateUrl: './catpage.component.html',
-//   styleUrls: ['./catpage.component.scss']
-// })
-// export class CatpageComponent {
-//   showBowl() {
-//     const bowl = document.querySelector('.bowl');
-//     if (bowl) {
-//       bowl.classList.remove('hidden');
-//       setTimeout(() => {
-//         bowl.classList.add('hidden');
-//       }, 3000)
 //     }
 //   }
 
+//   moveKitten() {
+//     const kitten = document.querySelector('.kitten');
+//     if (kitten) {
+//       // console.log('Kitten is moving down and scaling up');
+//       kitten.classList.add('move-down-400');
+//       kitten.classList.add('scale-up-30');
+//       // console.log(kitten);
+//     }
+//   }
+  
 // }
+
+
+
