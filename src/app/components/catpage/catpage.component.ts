3a07ef7389e346139f5698eb0c530125e6d4cae9
MagretBro <magretbro@gmail.com>
2024-03-29 11:39:03 +0700

    score save in localstorage and count and hidden showbowl after count is finished

--- src/app/components/catpage/catpage.component.ts
+++ src/app/components/catpage/catpage.component.ts
@@ -1,6 +1,5 @@
 
 
-
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 
@@ -10,6 +9,7 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./catpage.component.scss']
 })
 export class CatpageComponent implements OnInit {
+  score: number = 0;
 
   constructor(
     public datas: DataService
@@ -18,18 +18,16 @@ export class CatpageComponent implements OnInit {
   showBowlVisible: boolean = false;
   showScaleVisible: boolean = false;
 
-  ngOnInit(): void {}
-
-  feedCat() {
-    if(this.datas.counter >= 10) {
-      this.datas.counter -= 10;
-    }
+  ngOnInit(): void {
+    // Чтение значения score из localStorage при инициализации компонента
+    this.score = parseInt(localStorage.getItem('score') || '0');
+    console.log(this.score);
   }
-  
 
   toggleScaleVisibility() {
     this.showScaleVisible = !this.showScaleVisible;
   }
+
   showScale() {
     const scale = document.querySelector('.scale');
     if (this.datas.counter <= 0) {
@@ -39,12 +37,10 @@ export class CatpageComponent implements OnInit {
       }
   }
 
+  toggleBowlVisibility() {
+    this.showBowlVisible = !this.showBowlVisible;
+  }
 
-
-
-    toggleBowlVisibility() {
-      this.showBowlVisible = !this.showBowlVisible;
-    }
   showBowl() {
     const bowl = document.querySelector('.bowl');
     if (bowl) {
@@ -60,11 +56,106 @@ export class CatpageComponent implements OnInit {
     } else {
       console.log('Bowl element not found');
     }
-      }
+    //let score = parseInt(localStorage.getItem('score') || '0'); // Получаем текущее значение score из localStorage и парсим его в число
+    if (this.score >= 1) { // Проверяем, что score больше или равен 1
+      console.log(this.score--);
+      this.score--; // Уменьшаем score на 1
+      localStorage.setItem('score', this.score.toString()); // Записываем новое значение score в localStorage
+    }
+
+  }
 }
 
 
 
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { DataService } from 'src/app/services/data.service';
+
+// @Component({
+//   selector: 'app-catpage',
+//   templateUrl: './catpage.component.html',
+//   styleUrls: ['./catpage.component.scss']
+// })
+// export class CatpageComponent implements OnInit {
+//   score: string | null = null;
+
+//   constructor(
+//     public datas: DataService
+//      ) {}
+
+//   showBowlVisible: boolean = false;
+//   showScaleVisible: boolean = false;
+
+//   ngOnInit(): void {
+//     // Чтение значения score из localStorage при инициализации компонента
+//     this.score = localStorage.getItem('score');
+//     console.log(this.score);
+//   }
+
+
+//   // feedCat() {
+//   //   if(this.datas.counter >= 10) {
+//   //     this.datas.counter -= 10;
+//   //   }
+//   // }  
+
+//   toggleScaleVisibility() {
+//     this.showScaleVisible = !this.showScaleVisible;
+//   }
+//   showScale() {
+//     const scale = document.querySelector('.scale');
+//     if (this.datas.counter <= 0) {
+//       scale?.classList.remove('hidden');
+//       } else {
+//         console.log('datas element not found');
+//       }
+//   }
+
+//   toggleBowlVisibility() {
+//     this.showBowlVisible = !this.showBowlVisible;
+//   }
+
+//   showBowl() {
+//     const bowl = document.querySelector('.bowl');
+//     if (bowl) {
+//       bowl.classList.remove('hidden');
+//       const kitten = document.querySelector('.kitten');
+//       if (kitten) {
+//         kitten.classList.add('move-down-400');
+//         // kitten.classList.add('scale-up');
+//         console.log('Kitten is moved down and scaled up');
+//       } else {
+//         console.log('Kitten element not found');
+//       }
+//     } else {
+//       console.log('Bowl element not found');
+//     }
+//     let score = parseInt(localStorage.getItem('score') || '0'); // Получаем текущее значение score из localStorage и парсим его в число
+//     if (score >= 1) { // Проверяем, что score больше или равен 1
+//       score--; // Уменьшаем score на 1
+//       localStorage.setItem('score', score.toString()); // Записываем новое значение score в localStorage
+//     }
+
+//       }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// OOOOOLD VERSION
 
 // import { Component, OnInit } from '@angular/core';
 // import { DataService } from 'src/app/services/data.service';
