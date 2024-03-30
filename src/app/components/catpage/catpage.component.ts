0c165cedc404fd196d5cb72b12bdc14df8c549e8
MagretBro <magretbro@gmail.com>
2024-03-30 13:50:20 +0700

    catpage is realised count on the btn, change bowl, timeout

--- src/app/components/catpage/catpage.component.ts
+++ src/app/components/catpage/catpage.component.ts
@@ -16,7 +16,7 @@ export class CatpageComponent implements OnInit {
      ) {}
 
   showBowlVisible: boolean = false;
-  showScaleVisible: boolean = false;
+  // showScaleVisible: boolean = false; //  был исключен, т к не требуется
 
   ngOnInit(): void {
     // Чтение значения score из localStorage при инициализации компонента
@@ -24,49 +24,54 @@ export class CatpageComponent implements OnInit {
     console.log(this.score);
   }
 
-  toggleScaleVisibility() {
-    this.showScaleVisible = !this.showScaleVisible;
-  }
-
-  showScale() {
-    const scale = document.querySelector('.scale');
-    if (this.datas.counter <= 0) {
-      scale?.classList.remove('hidden');
-      } else {
-        console.log('datas element not found');
-      }
-  }
-
   toggleBowlVisibility() {
     this.showBowlVisible = !this.showBowlVisible;
   }
 
   showBowl() {
-    const bowl = document.querySelector('.bowl');
-    if (bowl) {
+    console.log('первый');
+    const bowl = document.querySelector('.bowl') as HTMLElement;
+    if (bowl && this.score > 0) {
+      bowl.style.backgroundImage = `url('/assets/img/bowl_full.png')`;
       bowl.classList.remove('hidden');
       const kitten = document.querySelector('.kitten');
       if (kitten) {
         kitten.classList.add('move-down-400');
-        // kitten.classList.add('scale-up');
-        console.log('Kitten is moved down and scaled up');
+       // console.log('Kitten is moved down and scaled up');
       } else {
-        console.log('Kitten element not found');
+        //console.log('Kitten element not found');
       }
     } else {
-      console.log('Bowl element not found');
+      //console.log('Bowl element not found');
     }
-    //let score = parseInt(localStorage.getItem('score') || '0'); // Получаем текущее значение score из localStorage и парсим его в число
     if (this.score >= 1) { // Проверяем, что score больше или равен 1
-      // console.log(this.score--);
-      console.log('score-1');
-
       this.score--; // Уменьшаем score на 1
       localStorage.setItem('score', this.score.toString()); // Записываем новое значение score в localStorage
+      //console.log('score-1');
     }
+    console.log('второй');
+    setTimeout(() => {
+      console.log('третий');
+      const bowl = document.querySelector('.bowl') as HTMLElement;
+      if (bowl) {
+        console.log('четвертый');
+        console.log(bowl.style.backgroundImage);
+        bowl.style.backgroundImage = `url('/assets/img/bowl_empty.png')`;
+        console.log(bowl.style.backgroundImage);
+        console.log(bowl.style.backgroundImage);
+      }
 
+      // if (this.score > 0) {
+      //   console.log('пятый');
+      //   setTimeout(() => {
+      //     console.log('шестой');
+      //     bowl.style.backgroundImage = `url('/assets/img/bowl_full.png')`;
+      //   }, 3000);
+      // }
+    }, 3000 );
   }
 }
+
 
 
 
@@ -83,38 +88,19 @@ export class CatpageComponent implements OnInit {
 //   styleUrls: ['./catpage.component.scss']
 // })
 // export class CatpageComponent implements OnInit {
-//   score: string | null = null;
+//   score: number = 0;
 
 //   constructor(
 //     public datas: DataService
 //      ) {}
 
 //   showBowlVisible: boolean = false;
-//   showScaleVisible: boolean = false;
+//   // showScaleVisible: boolean = false; //  был исключен, т к не требуется
 
 //   ngOnInit(): void {
 //     // Чтение значения score из localStorage при инициализации компонента
-//     this.score = localStorage.getItem('score');
+//     this.score = parseInt(localStorage.getItem('score') || '0');
 //     console.log(this.score);
-//   }
-
-
-//   // feedCat() {
-//   //   if(this.datas.counter >= 10) {
-//   //     this.datas.counter -= 10;
-//   //   }
-//   // }  
-
-//   toggleScaleVisibility() {
-//     this.showScaleVisible = !this.showScaleVisible;
-//   }
-//   showScale() {
-//     const scale = document.querySelector('.scale');
-//     if (this.datas.counter <= 0) {
-//       scale?.classList.remove('hidden');
-//       } else {
-//         console.log('datas element not found');
-//       }
 //   }
 
 //   toggleBowlVisibility() {
@@ -136,85 +122,15 @@ export class CatpageComponent implements OnInit {
 //     } else {
 //       console.log('Bowl element not found');
 //     }
-//     let score = parseInt(localStorage.getItem('score') || '0'); // Получаем текущее значение score из localStorage и парсим его в число
-//     if (score >= 1) { // Проверяем, что score больше или равен 1
-//       score--; // Уменьшаем score на 1
-//       localStorage.setItem('score', score.toString()); // Записываем новое значение score в localStorage
+//     //let score = parseInt(localStorage.getItem('score') || '0'); // Получаем текущее значение score из localStorage и парсим его в число
+//     if (this.score >= 1) { // Проверяем, что score больше или равен 1
+//       // console.log(this.score--);
+//       console.log('score-1');
+
+//       this.score--; // Уменьшаем score на 1
+//       localStorage.setItem('score', this.score.toString()); // Записываем новое значение score в localStorage
 //     }
 
-//       }
+//   }
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//// OOOOOLD VERSION
-
-// import { Component, OnInit } from '@angular/core';
-// import { DataService } from 'src/app/services/data.service';
-
-// @Component({
-//   selector: 'app-catpage',
-//   templateUrl: './catpage.component.html',
-//   styleUrls: ['./catpage.component.scss']
-// })
-// export class CatpageComponent implements OnInit {
-
-//   constructor(
-//     public datas: DataService
-//      ) {}
-
-//   showBowlVisible: boolean = false;
-//   showScaleVisible: boolean = false;
-
-//   ngOnInit(): void {
-//     // console.log(this.datas.counter)
-//     console.log(this.datas.roundPassed);
-
-//   }
-  
-
-//   toggleScaleVisibility() {
-//     this.showScaleVisible = !this.showScaleVisible;
-//   }
-//   showScale() {
-//     const scale = document.querySelector('.scale');
-//     if (this.datas.counter <= 0) {
-//       scale?.classList.remove('hidden');
-//       } else {
-//         console.log('datas element not found');
-//       }
-//   }
-
-
-
-
-//     toggleBowlVisibility() {
-//       this.showBowlVisible = !this.showBowlVisible;
-//     }
-//   showBowl() {
-//     const bowl = document.querySelector('.bowl');
-//     if (bowl) {
-//       bowl.classList.remove('hidden');
-//       const kitten = document.querySelector('.kitten');
-//       if (kitten) {
-//         kitten.classList.add('move-down-400');
-//         // kitten.classList.add('scale-up');
-//         console.log('Kitten is moved down and scaled up');
-//       } else {
-//         console.log('Kitten element not found');
-//       }
-//     } else {
-//       console.log('Bowl element not found');
-//     }
-//       }
-// }
